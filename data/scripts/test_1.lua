674503142a2822759258ec828d384fc3de3e67c3
Vertaalfout <104238697+Vertaalfout@users.noreply.github.com>
2022-07-01 19:31:25 -0400

    added some examples to experiment with in lua

--- data/scripts/test_1.lua
+++ data/scripts/test_1.lua
@@ -14,7 +14,8 @@ script.on_game_event("WEAPONS_FORCE_UPGRADE", false, forceUpgrade_weapons)
 --Every time the event "SHIELDS_FORCE_UPGRADE" runs, the function forceUpgrade_shields runs
 script.on_game_event("SHIELDS_FORCE_UPGRADE",false,forceUpgrade_shields)
 --the arguement false here means it does not happen again upon reload, IE saving and reloading
-
+function nothing()
+end
 
 
 --Stuff to work on later
@@ -33,83 +34,8 @@ function downgrade_enemy()
   --Hyperspace.ships.enemy.cloakSystem:UpgradeSystem(-20)
 end
 
-
---Test for drawing and stuff. No mouseover yet.
-
 mods.vals = {}--I think this stores initiallized values
 
-
-
-function boxBool_Toggle()
-    if mods.vals.testBoxBool then
-      mods.vals.testBoxBool = false
-      log('set to false')
-
-    else
-      mods.vals.testBoxBool = true
-      log('set to true')
-    end
-end
-
-script.on_game_event("DRAW", false, boxBool_Toggle)
-function nothing()
-end
-
---[[function test_before_rend()
-    if mods.vals.testBoxBool then
-      log('thingy before')
-      Graphics.CSurface.GL_DrawLine(6.0, 6.0, 100.0, 100.0, 3.0, Graphics.GL_Color(0.5, 0.5, 0.5, 1.0))
-      Graphics.freetype.easy_print(40, 400, 400, 'SEX');
-    else
-      log('no thingy before')
-    end
-end]]
---[[function test_after_rend()
-    if mods.vals.testBoxBool then
-      log('thingy after')
-      Graphics.CSurface.GL_DrawLine(6.0, 6.0, 100.0, 100.0, 5.0, Graphics.GL_Color(1.0, 0.2, 0.2, 1.0))
-      Graphics.freetype.easy_print(40, 400, 400, 'SEX 2');
-    else
-      log('no thingy after')
-    end
-end
-script.on_render_event(Defines.RenderEvents.LAYER_FRONT, nothing, test_after_rend)]]
-
-
-
-
-function rendertest2()
-    --if mods.vals.testBoxBool then
-      log('thingy after hello')
-      Graphics.CSurface.GL_DrawLine(6.0, 6.0, 100.0, 100.0, 5.0, Graphics.GL_Color(1.0, 0.2, 0.2, 1.0))
-      --Graphics.CSurface.GL_DrawLine(6.0, 6.0, 100.0, 100.0, 5.0, Graphics.GL_Color(1.0, 0.2, 0.2, 1.0))
-      --Graphics.freetype.easy_print(40, 400, 400, 'SEX 2')
-
-      Graphics.CSurface.GL_DrawRectOutline(300, 300, 400, 400, Graphics.GL_Color(0.4, 1.0, 0.2, 1.0), 2)
-      Graphics.freetype.easy_print(20, 350, 350, 'So long, and thanks for all the fish!')
-      Graphics.CSurface.GL_DrawLine(500, 100, 600, 100.0, 5.0, Graphics.GL_Color(0.4, 1.0, 0.2, 1.0))
-      Graphics.CSurface.GL_DrawLine(750, 750, 755, 755, 5.0, Graphics.GL_Color(0.4, 0.1, 1.0, 1.0))
-      Graphics.CSurface.GL_DrawTriangle(Hyperspace.Point(1,1), Hyperspace.Point(100,100), Hyperspace.Point(-200,-200), Graphics.GL_Color(1.0, 0.2, 0.2, 1.0))
-      log('thingy after goodbye')
-    --else
-      --log('no thingy after')
-    --end
-end
---script.on_render_event(Defines.RenderEvents.LAYER_PLAYER, nothing, rendertest2)
---[[function auto_repair_modhull()
-  Hyperspace.ships.player.oxygenSystem:PartialRepair(mods.vals.repair_oxygenSystem,true)
-  Hyperspace.ships.player.teleportSystem:PartialRepair(mods.vals.repair_teleportSystem,true)
-  Hyperspace.ships.player.cloakSystem:PartialRepair(mods.vals.repair_cloakSystem,true)
-  Hyperspace.ships.player.batterySystem:PartialRepair(mods.vals.repair_batterySystem ,true)
-  Hyperspace.ships.player.mindSystem:PartialRepair(mods.vals.repair_mindSystem ,true)
-  Hyperspace.ships.player.cloneSystem:PartialRepair(mods.vals.repair_cloneSystem ,true)
-  Hyperspace.ships.player.hackingSystem:PartialRepair(mods.vals.repair_hackingSystem ,true)
-  Hyperspace.ships.player.shieldSystem:PartialRepair(mods.vals.repair_shieldSystem ,true)
-  Hyperspace.ships.player.weaponSystem:PartialRepair(mods.vals.repair_weaponSystem ,true)
-  Hyperspace.ships.player.droneSystem:PartialRepair(mods.vals.repair_droneSystem ,true)
-  Hyperspace.ships.player.engineSystem:PartialRepair(mods.vals.repair_engineSystem ,true)"nil method"
-  Hyperspace.ships.player.medbaySystem:PartialRepair(mods.vals.repair_medbaySystem ,true) "nil method"
-end]]
 --[[SYS_SHIELDS,    //0
     SYS_ENGINES,    //1
     SYS_OXYGEN,     //2
@@ -169,14 +95,26 @@ end]]
     end
 --script.on_internal_event(Defines.InternalEvents.ON_TICK, repair_auto)
 
-function tyy()
-        log('hello')
-        --log(type(Hyperspace.Resources:CreateImagePrimitiveString("img/ship/forgemasterbase.png", 0, 0, 0, Graphics.GL_Color(0.5, 0.5, 0.5, 1.0), 1.0, false)))
-        Graphics.CSurface:GL_CreatePixelImagePrimitive("img/ship/forgemasterbase.png", 0, 0, 0, Graphics.GL_Color(0.5, 0.5, 0.5, 1.0), 1.0, false)
-        log('goodbye')
+
+--examples for graphics functions
+function D()
+
+  local y=Hyperspace.Resources:CreateImagePrimitiveString("ships_glow/zmt_boss_crystal_suicide_gib2.png", 0, 0, 0, Graphics.GL_Color(0.5, 0.5, 0.5, 1.0), 1.0, false)
+   Graphics.CSurface.GL_RenderPrimitive(y)
+   Graphics.CSurface.GL_DrawShield(100, 200, 300.0, 200.0, 1, 300, Graphics.GL_Color(0.5, 0.5, 0.5, 1.0), 5)
+   Graphics.CSurface.GL_DrawLine(6.0, 6.0, 100.0, 100.0, 5.0, Graphics.GL_Color(1.0, 0.2, 0.2, 1.0))
+   Graphics.CSurface.GL_DrawLine(6.0, 6.0, 100.0, 100.0, 5.0, Graphics.GL_Color(1.0, 0.2, 0.2, 1.0))
+   Graphics.freetype.easy_print(40, 400, 400, 'SEX 2')
+   Graphics.CSurface.GL_DrawRectOutline(300, 300, 400, 400, Graphics.GL_Color(0.4, 1.0, 0.2, 1.0), 2)
+   Graphics.freetype.easy_print(20, 350, 350, 'So long, and thanks for all the fish!')
+   Graphics.CSurface.GL_DrawLine(500, 100, 600, 100.0, 5.0, Graphics.GL_Color(0.4, 1.0, 0.2, 1.0))
+   Graphics.CSurface.GL_DrawLine(750, 750, 755, 755, 5.0, Graphics.GL_Color(0.4, 0.1, 1.0, 1.0))
+   Graphics.CSurface.GL_DrawTriangle(Hyperspace.Point(1,1), Hyperspace.Point(100,100), Hyperspace.Point(-200,-200), Graphics.GL_Color(1.0, 0.2, 0.2, 1.0))
 end
+--script.on_render_event(Defines.RenderEvents.LAYER_PLAYER, nothing, D)
 
 
+--test functions, may or may not work
 function beam()
   local projectile=Hyperspace.Damage()
   projectile.iShieldPiercing = 0
