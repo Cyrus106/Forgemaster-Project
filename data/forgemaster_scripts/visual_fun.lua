2da286034db194cfddd865eb99e2aeb3dc4a694c
Vertaalfout <104238697+Vertaalfout@users.noreply.github.com>
2022-09-14 23:20:28 -0400

    made the SHIP fucking INVISIBLE

--- data/forgemaster_scripts/visual_fun.lua
+++ data/forgemaster_scripts/visual_fun.lua
@@ -1,17 +1,12 @@
-mods.Forgemaster.invisibleShip=false
-
-
 script.on_render_event(Defines.RenderEvents.LAYER_PLAYER,
 function()
-  if mods.Forgemaster.invisibleShip then --if you want to tie this to a game variable just do
-                                        --if Hyperspace.ships.player:HasEquipment("game_variable_name")
+  if Hyperspace.ships.player:HasEquipment("fm_invisible_ship")>0 then
     Graphics.CSurface.GL_PushMatrix()
     Graphics.CSurface.GL_Scale(0, 0, 0)
   end
 end,
 function()
-  if mods.Forgemaster.invisibleShip then--if you want to tie this to a game variable just do
-                                        --if Hyperspace.ships.player:HasEquipment("game_variable_name")
+  if Hyperspace.ships.player:HasEquipment("fm_invisible_ship")>0 then
     Graphics.CSurface.GL_PopMatrix()
   end
 end)
