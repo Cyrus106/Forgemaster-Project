2e8dab9091f586357c4f5024297dc6f85299ef83
Vertaalfout <104238697+Vertaalfout@users.noreply.github.com>
2023-02-17 19:36:28 -0500

    patching errors

--- Inferno-Core/data/inferno_core_scripts/dependancy.lua
+++ Inferno-Core/data/inferno_core_scripts/dependancy.lua
@@ -1,16 +1,14 @@
-script.on_load(
-function()
-  --Creating a global variable with our addon's information,
-  --so that dependancies can be checked when the game loads
-  _G["INFERNO_CORE_INFO"]={
-      version="0.5",--No idea what number to put here
-      }
-  if _G["FORGEMASTER_INFO"] then
-    Hyperspace.ErrorMessage("Forgemaster was patched before Inferno-Core! Please re-patch your mods, and make sure to put Inferno-Core first!")
-  end
-
-  if _G["TCC_INFO"] then
-    Hyperspace.ErrorMessage("Trash Compactor Collection was patched before Inferno-Core! Please re-patch your mods, and make sure to put Inferno-Core first!")
-  end
+INFERNO_CORE_INFO = {
+  VERSION = {
+    MAJOR = 0,
+    MINOR = 5,
+    FEATURE = 0,
+  },
+}
+if FORGEMASTER_INFO then
+   error("Forgemaster was patched before Inferno-Core! Please re-patch your mods, and make sure to put Inferno-Core first!") 
 end
-)
+
+if TCC_INFO then
+  error("Trash Compactor Collection was patched before Inferno-Core! Please re-patch your mods, and make sure to put Inferno-Core first!")
+end
--- data/forgemaster_scripts/dependancy.lua
+++ data/forgemaster_scripts/dependancy.lua
@@ -1,14 +1,14 @@
 mods.Forgemaster = {}
---A more intuitive way of making sure the player patches dependancies first.
-script.on_load(
-function()
   --Creating a global variable with our addon's information,
   --so that dependancies can be checked when the game loads
-  _G["FORGEMASTER_INFO"]={
-      version="0.4.1",
-      }
-  if not _G["INFERNO_CORE_INFO"] then
-    Hyperspace.ErrorMessage("Warning! Inferno-Core was not patched before Forgemaster! Please re-patch your mods, or the game will not work!")
-  end
+FORGEMASTER_INFO = {
+  VERSION = {
+    MAJOR = 0,
+    MINOR = 5,
+    FEATURE = 0,
+  },
+}
+if not INFERNO_CORE_INFO then 
+  error("Warning! Inferno-Core was not patched before Forgemaster! Please re-patch your mods, or the game will not work!") 
 end
-)
+
