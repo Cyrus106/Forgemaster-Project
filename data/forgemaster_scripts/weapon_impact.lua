6d63f0b6ee87bef3358666278836bb5c5695137e
Vertaalfout <104238697+Vertaalfout@users.noreply.github.com>
2023-04-18 15:05:38 -0400

    Update weapon_impact.lua

--- data/forgemaster_scripts/weapon_impact.lua
+++ data/forgemaster_scripts/weapon_impact.lua
@@ -87,13 +87,19 @@ function(ShipManager, Projectile, Location, Damage, realNewTile, beamHitType)
 end)
 
 
+local bombBeams = {
+  BEAM_NAME = "BOMB_NAME"
+}
+
 --Tile based bombs
---[[
+---[[
 script.on_internal_event(Defines.InternalEvents.DAMAGE_BEAM,
 function(ShipManager, Projectile, Location, Damage, realNewTile, beamHitType)
-  if beamHitType ~= Defines.BeamHit.SAME_TILE then
+  local bomb
+  pcall(function() bomb = bombBeams[Hyperspace.Get_Projectile_Extend(Projectile).name] end)
+  if beamHitType ~= Defines.BeamHit.SAME_TILE and bomb then
     local SpaceManager = Hyperspace.Global.GetInstance():GetCApp().world.space
-    local blueprint = Hyperspace.Global.GetInstance():GetBlueprints():GetWeaponBlueprint("BOMB_1")
+    local blueprint = Hyperspace.Global.GetInstance():GetBlueprints():GetWeaponBlueprint(bomb)
     local bombOwner = (ShipManager.iShipId + 1) % 2
     local target = Hyperspace.Pointf(Location.x // 35 * 35 + 17.5, Location.y // 35 * 35 + 17.5)
     local targetSpace = ShipManager.iShipId
