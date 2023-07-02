97a430744b5b539b4beb3477bfd269c63e245055
Vertaalfout <104238697+Vertaalfout@users.noreply.github.com>
2023-07-02 18:54:45 -0400

    Update for HS 1.5

--- Inferno-Core/data/inferno_core_scripts/weapon_fire.lua
+++ Inferno-Core/data/inferno_core_scripts/weapon_fire.lua
@@ -3,15 +3,16 @@ local real_projectile = mods.inferno.real_projectile
 local randomInt = mods.inferno.randomInt
 
 
-script.on_fire_event(Defines.FireEvents.WEAPON_FIRE,
-function(ship, weapon, projectile)
-  if ship:GetAugmentationValue("WEAPON_LOCKDOWN") > 0 and real_projectile(projectile) then 
+script.on_internal_event(Defines.InternalEvents.PROJECTILE_FIRE,
+function(projectile, weapon)
+  if weapon:GetAugmentationValue("WEAPON_LOCKDOWN") > 0 and real_projectile(projectile) then 
     projectile.damage.bLockdown = true
   end
 end)
 
-local function beam_pierce(ship, _, projectile)
-  local beam_pierce_modifier = ship:GetAugmentationValue("AUG_BEAM_PIERCE")
+script.on_internal_event(Defines.InternalEvents.PROJECTILE_FIRE,
+function(projectile, weapon)
+  local beam_pierce_modifier = weapon:GetAugmentationValue("AUG_BEAM_PIERCE")
   if projectile:GetType() == 5 then
     --conditional fix for weapons with negative damage and nonnegative shield piercing
     if projectile.damage.iShieldPiercing >= 0 then
@@ -19,10 +20,7 @@ local function beam_pierce(ship, _, projectile)
     end
     projectile.damage.iShieldPiercing = projectile.damage.iShieldPiercing + beam_pierce_modifier
   end
-end
-
-script.on_fire_event(Defines.FireEvents.WEAPON_FIRE, beam_pierce)
-script.on_fire_event(Defines.FireEvents.ARTILLERY_FIRE, beam_pierce)
+end)
 
 local function GetRandomPoint(center, radius)
   local radius = radius * (Hyperspace.random32() / 2147483647)
@@ -37,26 +35,20 @@ end
 
 if getmetatable(Hyperspace.WeaponBlueprint)['.instance']['.get']['radius'] then --if WeaponBlueprint::radius is accessible.
 
-  script.on_fire_event(Defines.FireEvents.WEAPON_FIRE,
-  function(ship, weapon, projectile)
-    if ship:HasAugmentation("RADIUS_REDUCTION") > 0 then
+  script.on_internal_event(Defines.InternalEvents.PROJECTILE_FIRE,
+  function(projectile, weapon)
+    if weapon:HasAugmentation("RADIUS_REDUCTION") > 0 then
       projectile.target.x, projectile.target.y = GetRandomPoint(weapon.lastTargets[0], weapon.radius)
     end
   end)
 
-  script.on_internal_event(Defines.InternalEvents.ON_TICK,
-  function()
-    for i = 0, 1 do 
-      local ShipManager = Hyperspace.Global.GetInstance():GetShipManager(i)
-      if ShipManager then
+  script.on_internal_event(Defines.InternalEvents.SHIP_LOOP,
+  function(ShipManager)
         local weaponSystem = ShipManager.weaponSystem
         if weaponSystem then
           for weapon in vter(weaponSystem.weapons) do
             weapon.radius = math.max(0, weapon.blueprint.radius - ShipManager:GetAugmentationValue("RADIUS_REDUCTION"))
           end
         end
-      end
-    end
   end)
-
 end
--- data/forgemaster_scripts/weapon_fire.lua
+++ data/forgemaster_scripts/weapon_fire.lua
@@ -2,19 +2,23 @@ local vter = mods.inferno.vter
 local real_projectile = mods.inferno.real_projectile
 local randomInt = mods.inferno.randomInt
 
+local redShot = Hyperspace.Resources:GetImageId "weapon_projectiles/fm_surge_laser_proj_red.png"
+local blueShot = Hyperspace.Resources:GetImageId "weapon_projectiles/fm_surge_laser_proj_blue.png"
 
-
-script.on_fire_event(Defines.FireEvents.WEAPON_FIRE,
-function(ship, weapon, projectile)
-  local otherShipId = 1 - ship.iShipId
-  local otherShip = Hyperspace.Global.GetInstance():GetShipManager(otherShipId)
-  if weapon.name == "'Multiplicity' Barrage Laser" then
-    local projectile_number = (weapon.numShots - weapon.queuedProjectiles:size())%weapon.numShots --modulus for when weapon fires fast enough to queue more shots
-      if projectile_number <= 5 then --projectiles that aim towards the targetted system, change visual here when possible
+script.on_internal_event(Defines.InternalEvents.PROJECTILE_FIRE,
+function(projectile, weapon)
+  local otherShip = Hyperspace.Global.GetInstance():GetShipManager(1 - projectile:GetOwnerId())
+  if weapon.blueprint.name == "FM_SURGE_LASER" then
+    local projectile_number = (weapon.numShots - weapon.queuedProjectiles:size()) % weapon.numShots --modulus for when weapon fires fast enough to queue more shots
+      if projectile_number <= 5 then --projectiles that aim towards the targetted system
+        projectile.flight_animation.animationStrip = redShot
+        projectile.speed_magnitude = projectile.speed_magnitude * 1.4
       elseif projectile_number <= 12 then --make seven projectiles hit a random system
         local sys_list = otherShip.vSystemList
         local system_target = sys_list[Hyperspace.random32() % sys_list:size()]
         projectile.target = system_target.location
+        projectile.flight_animation.animationStrip = blueShot
+        projectile.speed_magnitude = projectile.speed_magnitude * 1.2
       else  --make the rest hit a random room
         projectile.target = otherShip:GetRandomRoomCenter()
       end
@@ -23,6 +27,8 @@ function(ship, weapon, projectile)
       projectile.position.x = projectile.position.x + randomInt(-12, 12)
       projectile.position.y = projectile.position.y + randomInt(-7, 7)
   end
+
+  return Defines.Chain.CONTINUE
 end)
 
 local shotTextures = {
@@ -31,9 +37,8 @@ local shotTextures = {
   [2] = Hyperspace.Resources:GetImageId "weapon_projectiles/charge_sniper_3.png",
   [3] = Hyperspace.Resources:GetImageId "weapon_projectiles/charge_sniper_4.png",
 }
-
-script.on_fire_event(Defines.FireEvents.WEAPON_FIRE,
-function(ship, weapon, projectile)
+script.on_internal_event(Defines.InternalEvents.PROJECTILE_FIRE,
+function(projectile, weapon)
   if weapon.blueprint.name == "FM_CHARGE_SNIPER" then
     local boost = weapon.queuedProjectiles:size()
     projectile.damage.iDamage = projectile.damage.iDamage + (2 * boost)
@@ -41,8 +46,6 @@ function(ship, weapon, projectile)
     projectile.speed_magnitude = projectile.speed_magnitude * (1 + (boost / 2))
     projectile.damage.breachChance = math.floor(2.5 * (boost + 1))
     weapon.queuedProjectiles:clear()
-    print(projectile.flight_animation.animationStrip)
     projectile.flight_animation.animationStrip = shotTextures[boost]
-    print(projectile.flight_animation.animationStrip)
   end
 end)
